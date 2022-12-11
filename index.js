4a4037970b66e7761b50ee803245d2b9e11f2c75
xxjamisxx <gui_haunter@hotmail.com>
2022-12-10 22:24:32 -0300

    atualizaçãoo

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post("/webhook", (req,res) => {
                  
                 axios({
                     method: "POST",
-                    url:"https://graph.facebook.com/v15.0/"+phon_no_id+"/message?access_token="+token,
+                    url:"https://graph.facebook.com/v15.0/"+phon_no_id+"/messages?access_token="+token,
                     data:{
                         messaging_product:"whatsapp",
                         to:from,
